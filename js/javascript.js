c025571dc1ad79599ba527f32cbcb38e91203227
Abdullah Toqeer <toqeerabdullah776@gmail.com>
2022-04-20 11:10:10 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,4 @@
-
+let map = L.map('map').setView([58.373523, 26.716045], 12)
+const osm =
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19, attribution: 'OpenStreetMap contributors',})
+osm.addTo(map)
