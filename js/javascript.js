d208c5f9e87d10caccdfd9f31c53c93b74600a60
Abdullah Toqeer <toqeerabdullah776@gmail.com>
2022-04-21 15:03:44 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,10 @@
 let map = L.map('map').setView([58.373523, 26.716045], 12)
 const osm =
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19, attribution: 'OpenStreetMap contributors',})
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 20
+})
 osm.addTo(map)
 
 // add popup to each feature
