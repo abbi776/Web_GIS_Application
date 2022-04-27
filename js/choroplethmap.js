6d5ca41f9a4cabfddc8aa679f40d848d4a7de751
Abdullah Toqeer <toqeerabdullah776@gmail.com>
2022-04-27 10:37:49 +0300

    Create choroplethmap.js

---
+++ js/choroplethmap.js
@@ -0,0 +1,37 @@
+// creating and adding map feature
+
+let map = L.map('map').setView([58.373523, 26.716045], 12)
+const osm =
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 20
+})
+osm.addTo(map)
+
+addGeoJson('geojson/tartu_city_districts_edu.geojson')
+
+// add geoJSON layer
+async function addGeoJson(url) {
+  const response = await fetch(url)
+  const data = await response.json()
+  L.choropleth(data, {
+    valueProperty: 'TOWERS', // which property in the features to use
+    scale: ['#ffffff', '#ff9900'], // chroma.js scale - include as many as you like
+    steps: 5, // number of breaks or steps in range
+    mode: 'q', // q for quantile, e for equidistant, k for k-means
+    style: {
+      color: '#fff', // border color
+      weight: 2,
+      fillOpacity: 0.8,
+    },
+    onEachFeature: function (feature, layer) {
+      layer.bindPopup('Value: ' + feature.properties.TOWERS)
+    },
+  }).addTo(map)
+}
+
+// default map settings for default view button
+function defaultMapSettings() {
+map.setView([58.373523, 26.716045], 12)
+}
